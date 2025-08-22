596b86f25e92348c446e42f9b1c568e36aeb9cb6
Hoang Quan <siluq.quan.1609@gmail.com>
2025-08-22 18:03:09 +0700

    Thuc Hanh Day02

---
+++ WebApp22_08/WebApp22_08/Controllers/CustomerController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp22_08.Controllers
+{
+    public class CustomerController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.Customers = new List<string> { "Nguyễn Văn A", "Nguyễn Thị B", "Lê Văn C" };
+            return View();
+        }
+
+        public IActionResult Detail(int id)
+        {
+            ViewBag.Customer = "Thông tin chi tiết khách hàng có ID = " + id;
+            return View();
+        }
+    }
+}
---
+++ WebApp22_08/WebApp22_08/Controllers/ProductController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp22_08.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Detail(int id)
+        {
+            ViewBag.Product = "Chi tiết sản phẩm có ID = " + id;
+            return View();
+        }
+    }
+}
