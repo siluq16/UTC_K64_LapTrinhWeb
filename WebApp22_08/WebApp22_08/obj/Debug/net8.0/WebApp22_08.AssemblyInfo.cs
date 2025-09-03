30cd28455a8ddde1e4c0017d9c35858e7dfb3d60
Hoang Quan <siluq.quan.1609@gmail.com>
2025-09-03 17:27:36 +0700

    Bai Thuc Hanh Day03

--- WebApp22_08/WebApp22_08/obj/Debug/net8.0/WebApp22_08.AssemblyInfo.cs
+++ WebApp22_08/WebApp22_08/obj/Debug/net8.0/WebApp22_08.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApp22_08")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+596b86f25e92348c446e42f9b1c568e36aeb9cb6")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApp22_08")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApp22_08")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
