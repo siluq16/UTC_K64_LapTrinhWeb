cb578079f6e010129645e6bb2f0ed46d678a85ca
Hoang Quan <siluq.quan.1609@gmail.com>
2025-09-17 13:17:36 +0700

    Thuc Hanh Day 04

---
+++ Day04Layout/Day04Layout/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Day04Layout.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
